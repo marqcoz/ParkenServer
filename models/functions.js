912f7a49a40acdac1a93e42d22b3d1a44e53dcf9
marqcoz <marqc.oz@gmail.com>
2018-12-05 11:04:16 -0600

    cool

--- models/functions.js
+++ models/functions.js
@@ -1333,7 +1333,7 @@ functions.obtenerSesionesParken = function(automovilista, callback){
           ' AND idzonaparken != 1'+
           //'AND s.estatus = \'PENDIENTE\'' +
           'AND sp.automovilista_idautomovilista = ' + automovilista +
-          ') ORDER BY fechainicio ASC';
+          ') ORDER BY fechainicio DESC';
 
       //console.log(query);
 
