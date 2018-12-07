35c83a3a00ab79cc24b31e3edddda736f4dabdf2
marqcoz <marqc.oz@gmail.com>
2018-12-06 18:44:51 -0600

    q

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ io.on('connection', function(socket){
   socket.on('disponibleAReportes', function(loc){
     console.log('ID');
     console.log(socket.id);
-    var jsonLoc = JSON.stringify(loc);
+    console.log(loc.idSupervisor.toString);
     //Creamos el json con la información del supervisor
     var jsonLocation = {
       id: jsonLoc.idSupervisor,
