dc099e01d22cf282fe7df7462eeaf3ea0ab4cad3
marqcoz <marqc.oz@gmail.com>
2018-12-11 20:34:47 -0600

    r

--- routes/Requests.js
+++ routes/Requests.js
@@ -3781,8 +3781,8 @@ app.post("/supervisor/obtenerTodosReportes", function(req, res){
 					'"apellidoautomovilista" :"' + data.rows[i].apellido + '", ' +
 					'"emailautomovilista" :"' + data.rows[i].email + '", ' +
 					'"celularautomovilista" :"' + data.rows[i].celular + '", ' +
-					'"puntosparken" :' + data.rows[i].puntosparken; //', ' +
-					//'"token" :"' + data.rows[i].token + '"';
+					'"puntosparken" :' + data.rows[i].puntosparken + ', ' +
+					'"token" :"' + data.rows[i].token + '"';
 
 					if(i === data.rowCount - 1){	
 						jeison = jeison + '}'; 
